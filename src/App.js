9a8950782057f4289ff544e3f099cfe401c4cdd0
Piotr Glodek <pioglodek@gmail.com>
2020-09-22 21:13:42 +0200

    cart props delivering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Providers from './Providers';
 import { Navigation } from './components';
 
 function App() {
+  const [cartProducts, setProduct] = useState([]);
+  const cartItemsCount = cartProducts.length;
+  /*
+  const addProductToCart = (product) => {
+    setProduct((prevArray) => [...prevArray, product]);
+  };
+  const removeProductFromCart = (productToDelete) => {
+    console.log(cartProducts);
+    const newProducts = cartProducts.filter(
+      (product) => product.productName !== productToDelete.productName
+    );
+    setProduct(newProducts);
+  };
+  */
+  const cartProps = {
+    cartItemsCount: cartItemsCount,
+  };
   return (
     <Providers>
-      <Navigation />
+      <Navigation cartProps={cartProps} />
     </Providers>
   );
 }
