afb36669f587a6989ea5c0e79c267a755fe09a9a
Piotr Glodek <pioglodek@gmail.com>
2020-09-26 19:26:23 +0200

    loading website function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import Providers from './Providers';
+import React, { useContext } from 'react';
+
 import { Switch, Route } from 'react-router-dom';
 // components
 import { Navigation } from './components';
 // pages
 import { Home } from './pages';
+// context
+import { ShopContext } from './store';
 
 function App() {
+  const [state] = useContext(ShopContext);
+  const { loading } = state;
+  if (loading) {
+    return <p>loading...</p>;
+  }
   return (
-    <Providers>
+    <>
       <Navigation />
       <Switch>
         <Route exact path='/' component={Home} />
       </Switch>
-    </Providers>
+    </>
   );
 }
 
