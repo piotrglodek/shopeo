57f0a34166ec8f284d7d2a3c04ecca5282d578e3
Piotr Glodek <pioglodek@gmail.com>
2020-09-26 19:28:35 +0200

    deleted loading condition

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,17 +5,13 @@ import { ProductSection } from '../../components';
 
 function Home() {
   const [state] = useContext(ShopContext);
-  const { categories, shoes, loading } = state;
+  const { categories, shoes } = state;
 
-  if (loading) {
-    return <Container>Loading...</Container>;
-  } else {
-    const productsSections = categories.map((category) => {
-      const { name } = category;
-      return <ProductSection key={name} title={name} shoes={shoes} />;
-    });
-    return <Container>{productsSections}</Container>;
-  }
+  const productsSections = categories.map((category) => {
+    const { name } = category;
+    return <ProductSection key={name} title={name} shoes={shoes} />;
+  });
+  return <Container>{productsSections}</Container>;
 }
 
 export default Home;
