3d9039577cd4706754f4ed5e42831b4e744861cb
Piotr Glodek <pioglodek@gmail.com>
2020-09-26 21:46:23 +0200

    del unused varaible

--- src/pages/CategoryPage/CategoryPage.js
+++ src/pages/CategoryPage/CategoryPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, useRouteMatch } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 // components
 import { Grid, GridItem } from '../../styled/shared';
 import { Product, ProductSection } from '../../components';
