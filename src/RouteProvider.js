6de52d7f8c2f0cb6be33070a95ab63c1bf4e2d4e
Piotr Glodek <pioglodek@gmail.com>
2020-09-29 14:31:31 +0200

    Providers

--- src/RouteProvider.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-function RouteProvider({ children }) {
-  return <Router>{children}</Router>;
-}
-
-export default RouteProvider;
